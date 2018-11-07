2f276c6a8c02763a3f54bce97e64dc4acc7fcd22
gyp <gyp@gypdeMacBook-Pro.local>
2018-11-07 20:15:06 +0800

    add city list ajax

---
+++ src/pages/city/components/List.vue
@@ -0,0 +1,98 @@
+<template>
+    <div class="list" ref="wrapper">
+        <div>
+            <div class="area">
+                <div class="title border-topbottom">当前城市</div>
+                <div class="button-list">
+                    <div class="button-wrapper">
+                            <div class="button">北京</div>
+                    </div>
+                </div>
+            </div>
+            <div class="area">
+                <div class="title border-topbottom">热门城市</div>
+                <div class="button-list">
+                    <div 
+                        class="button-wrapper"
+                        v-for="item of hot"
+                        :key="item.id"
+                    >
+                            <div class="button">{{item.name}}</div>
+                    </div>                  
+                </div>
+            </div>
+            <div class="area" v-for="(item, key) of cities" :key="key">
+                <div class="title border-topbottom">{{key}}</div>
+                <div class="item-list">
+                    <div 
+                        class="item border-bottom"
+                         v-for="city of item" 
+                         :key="city.id"
+                    >
+                        {{city.name}}
+                    </div>
+                  
+                </div>
+            </div>
+        </div>    
+    </div>
+</template>
+
+<script>
+import Bscroll from 'better-scroll'
+export default {
+    name: 'CityList',
+    mounted () {
+        this.scroll = new Bscroll(this.$refs.wrapper)
+    },
+    props: {
+        cities: Object,
+        hot: Array
+    }
+}
+</script>
+
+<style lang="stylus" scoped>
+    @import '~styles/iconfont/varibles.styl'
+    .border-topbottom
+        &:before
+            border-color: #ccc
+        &:after
+            border-color: #ccc
+    .border-bottom
+            &:before
+                border-color: #ccc
+    .list
+        position: absolute 
+        top: 0
+        bottom: 0
+        left: 0
+        right: 0
+        xbackground: pink
+        overflow: hidden
+        top: 1.58rem
+    .title
+        line-height: .54rem
+        background: #eee
+        padding: left
+        color: #666
+        font-size: .26rem 
+    .button-list
+        padding: .1rem .6rem .1rem .1rem
+        overflow: hidden
+        .button-wrapper
+            width: 33.33%
+            float: left  
+            .button
+                text-align: center
+                margin: .1rem
+                border: .02rem solid #ccc
+                color: #666
+                padding: .1rem 0
+                border-radius: .06rem
+    .item-list
+        .item
+            line-height: .76rem
+            padding-left: .2rem
+        
+</style>
